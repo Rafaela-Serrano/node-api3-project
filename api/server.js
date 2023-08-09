299455b4e82951e7a6b587dd2f604a55a7f88fd7
Rafaela Serrano <raphaela.alonsoserrano@gmail.com>
2023-08-09 15:16:45 -0500

    Middlewares created with there respective methods

--- api/server.js
+++ api/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const morgan = require('morgan');
+const {logger} = require('./middleware/middleware');
+const usersRouter = require('./users/users-router');
 
 const server = express();
 
-// remember express by default cannot parse JSON in request bodies
+// remember express by default cannot parse JSON in request bodies:
+server.use(express.json());
 
-// global middlewares and the user's router need to be connected here
+// global middlewares and the user's router need to be connected here:
+server.use('/api/users', usersRouter);
+server.use(morgan('dev'));
+server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
